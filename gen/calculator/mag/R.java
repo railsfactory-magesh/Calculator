70cc95e5352cabf7953d0dc98ed6779d08fa0317
Magesh <magesh.s@sedin.co.in>
2011-02-22 18:23:47 +0530

    modified the button layouts to look like the usual calc

--- gen/calculator/mag/R.java
+++ gen/calculator/mag/R.java
@@ -14,22 +14,22 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button0=0x7f05000a;
+        public static final int button0=0x7f05000d;
         public static final int button1=0x7f050001;
         public static final int button2=0x7f050002;
         public static final int button3=0x7f050003;
-        public static final int button4=0x7f050004;
-        public static final int button5=0x7f050005;
-        public static final int button6=0x7f050006;
-        public static final int button7=0x7f050007;
-        public static final int button8=0x7f050008;
-        public static final int button9=0x7f050009;
-        public static final int buttonc=0x7f050010;
-        public static final int div=0x7f05000e;
+        public static final int button4=0x7f050005;
+        public static final int button5=0x7f050006;
+        public static final int button6=0x7f050007;
+        public static final int button7=0x7f050009;
+        public static final int button8=0x7f05000a;
+        public static final int button9=0x7f05000b;
+        public static final int buttonc=0x7f050004;
+        public static final int div=0x7f050008;
         public static final int equal=0x7f05000f;
         public static final int minus=0x7f05000c;
-        public static final int mul=0x7f05000d;
-        public static final int plus=0x7f05000b;
+        public static final int mul=0x7f05000e;
+        public static final int plus=0x7f050010;
         public static final int tf=0x7f050000;
     }
     public static final class layout {
